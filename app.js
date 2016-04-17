8b4a831e7208bb3a75f03d7a8d2069cfb308ab2c
ravenusmc <mcuddy77@gmail.com>
2016-04-17 15:54:41 -0400

    did styling for pictures

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //1. These two statements I did first 
 var express = require("express");
 var app = express();
+var bodyParser = require("body-Parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 //4. I then added this to see my ejs pages at the routes below. 
 app.set("view engine", "ejs");
+
+  var campgrounds = [
+    {name: "Hampshire Hills", image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?crop=entropy&dpr=2&fit=crop&fm=jpg&h=625&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1200"},
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1436285122087-89584a1d9398?crop=entropy&dpr=2&fit=crop&fm=jpg&h=625&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1200"},
+    {name: "Cuddy Creek", image: "https://images.unsplash.com/photo-1455496231601-e6195da1f841?crop=entropy&dpr=2&fit=crop&fm=jpg&h=625&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1200"}
+  ];
 
 //3. This is for the landing page. 
 app.get("/", function(req,res) {
@@ -12,13 +20,27 @@ app.get("/", function(req,res) {
 
 app.get("/campgrounds", function(req,res) {
   //This array is only temporary
-  var campgrounds = [
-    {name: "Hampshire Hills", image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?crop=entropy&dpr=2&fit=crop&fm=jpg&h=625&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1200"},
-    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1436285122087-89584a1d9398?crop=entropy&dpr=2&fit=crop&fm=jpg&h=625&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1200"},
-    {name: "Cuddy Creek", image: "https://images.unsplash.com/photo-1455496231601-e6195da1f841?crop=entropy&dpr=2&fit=crop&fm=jpg&h=625&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1200"}
-  ]
+
   //The object, {}, is needed to pass in the campgrounds arrary. 
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+//5 This is for added new campgrounds-notice the post.
+//Be sure to do npm install body-parser --save, look at var bodyParser above
+app.post("/campgrounds", function(req,res){
+  //Below line to test after you have it set up. 
+  //res.send("Submitted!");
+  //get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image; 
+  var newCampground = {name:name, image: image};
+  campgrounds.push(newCampground)
+  //reditect back to campgrounds page
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req,res) {
+  res.render("new.ejs");
 });
 
 
@@ -26,4 +48,35 @@ app.get("/campgrounds", function(req,res) {
 app.listen(3000, function() {
   console.log("Head to LocalHost 3000!");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
